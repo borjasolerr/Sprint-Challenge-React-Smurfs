9513b1bcd82a6e1743a6538c277fb94ce38d63c3
borjasolerr <borjasolrey@gmail.com>
2019-03-01 11:07:48 +0100

    Constructed an AXIOS request to POST to add a Smurf

--- village/src/App.js
+++ village/src/App.js
@@ -8,29 +8,54 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      smurfs: []
+      smurfs: [],
+      postSuccess: '',
+      postFail: ''
     };
   }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
 
-  componentDidMount() {
+  postSmurf = smurf => {
+    axios
+      .post('http://localhost:3333/smurfs', smurf)
+      .then(res => {
+        console.log(res);
+        this.setState({
+          postSuccess: 'Smurf Added!',
+          postFail: ''
+        });
+
+        this.setState({ smurfs: res.data });
+      })
+      .catch(err => {
+        this.setState({
+          postSuccess: '',
+          postFail: 'Add Failed :('
+        });
+      });
+  };
+
+  getSmurfs = () => {
     axios
       .get('http://localhost:3333/smurfs')
       .then(res => {
-        console.log(res);
         this.setState({
           smurfs: res.data
         });
       })
       .catch(err => console.log(err));
+  };
+
+  componentDidMount() {
+    this.getSmurfs();
   }
 
   render() {
     return (
       <div className="App">
-        <SmurfForm />
+        <SmurfForm postSmurf={this.postSmurf} />
         <Smurfs smurfs={this.state.smurfs} />
       </div>
     );
--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -13,13 +13,15 @@ class SmurfForm extends Component {
   addSmurf = event => {
     event.preventDefault();
     // add code to create the smurf using the api
+    //console.log('state passed', this.state);
+    this.props.postSmurf(this.state);
 
     this.setState({
       name: '',
       age: '',
       height: ''
     });
-  }
+  };
 
   handleInputChange = e => {
     this.setState({ [e.target.name]: e.target.value });
@@ -29,24 +31,9 @@ class SmurfForm extends Component {
     return (
       <div className="SmurfForm">
         <form onSubmit={this.addSmurf}>
-          <input
-            onChange={this.handleInputChange}
-            placeholder="name"
-            value={this.state.name}
-            name="name"
-          />
-          <input
-            onChange={this.handleInputChange}
-            placeholder="age"
-            value={this.state.age}
-            name="age"
-          />
-          <input
-            onChange={this.handleInputChange}
-            placeholder="height"
-            value={this.state.height}
-            name="height"
-          />
+          <input onChange={this.handleInputChange} placeholder="name" value={this.state.name} name="name" />
+          <input onChange={this.handleInputChange} placeholder="age" value={this.state.age} name="age" />
+          <input onChange={this.handleInputChange} placeholder="height" value={this.state.height} name="height" />
           <button type="submit">Add to the village</button>
         </form>
       </div>
